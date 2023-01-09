e219c695560687e5437ed3b76458b6bc2c52332f
MickaelMarseul <mick.marseul@gmail.com>
2023-01-09 11:42:20 +0000

    basic

--- lib/src/string_tools.dart
+++ lib/src/string_tools.dart
@@ -1,3 +1,16 @@
 extension StringTools on String {
   bool get isValidURL => Uri.tryParse(this) != null;
+
+  bool IsValidPass(String myPass) {
+    //final RegExp regExpNumber = new RegExp(r"^[0-9]{5}$");
+    final RegExp regExpPass = new RegExp(
+        r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{9}$");
+
+    return regExpPass.hasMatch(myPass);
+  }
+
+  bool IsValidNumber(String myNumber) {
+    final RegExp regExpNumber = new RegExp(r"^[0-9]{5}$");
+    return regExpNumber.hasMatch(myNumber);
+  }
 }
