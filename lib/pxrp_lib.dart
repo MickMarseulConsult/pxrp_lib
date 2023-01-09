b8341dbc770046520c68c211faaae876fc75d32f
MickaelMarseul <mick.marseul@gmail.com>
2023-01-09 11:04:55 +0000

    ToolsInit

---
+++ lib/pxrp_lib.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pxrp_lib;
+
+export 'src/string_tools.dart';
+
+// TODO: Export any libraries intended for clients of this package.
